1309626c7fc7dfbf07552fa14ff2ab5f17213850
1234qwer926 <srao50872@gmail.com>
2025-03-09 20:53:35 +0530

    final changes

--- FinanceMangmentBackend/middleware/authMiddleware.js
+++ FinanceMangmentBackend/middleware/authMiddleware.js
@@ -3,18 +3,18 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
     const authHeader = req.header("Authorization"); 
-    console.log("Received Header:", authHeader);
+    // console.log("Received Header:", authHeader);
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ message: "No token, authorization denied" });
     }
 
     const token = authHeader.split(" ")[1]; 
-    console.log("Extracted Token:", token); 
+    // console.log("Extracted Token:", token); 
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("Decoded Token:", decoded); 
+        // console.log("Decoded Token:", decoded); 
         req.user = decoded; 
         next();
     } catch (error) {
